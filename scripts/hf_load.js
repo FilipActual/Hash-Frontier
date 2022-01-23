3bcd88301915fef4d15a187a07963c2b0fb6c9ba
Justin Filip <justin.filip@outlook.com>
2022-01-23 11:24:21 -0700

    Add files via upload

---
+++ scripts/hf_load.js
@@ -0,0 +1,9 @@
+// if (document.location.protocol != "https:") {
+
+//     document.location.href = "https://www.hashfrontier.com" + document.location.pathname;
+
+// };
+
+const hf_logo = document.getElementById("hf_logo");
+
+hf_logo.className = "hf_logo_finish";
